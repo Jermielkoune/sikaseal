cdf80f288f7cb6b61ff8939e363586f61ba6fd55
jermiel <kpessou-jermiel.kounouho@epitech.eu>
2026-02-13 15:10:20 +0100

    add SikaEngineApplication and corresponding test class

---
+++ src/test/java/com/sikaseal/sika_engine/SikaEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sikaseal.sika_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SikaEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
