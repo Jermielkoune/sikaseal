173e88c9a24d3d04785054ff0517362a5a7e9e17
KOUNOUHO Kpessou Jermiel <kpessou-jermiel.kounouho@epitech.eu>
2026-02-15 20:54:34 +0100

    refactor SikaEngineApplication and SikaEngineApplicationTests for code consistency

--- src/main/java/com/sikaseal/sika_engine/SikaEngineApplication.java
+++ src/main/java/com/sikaseal/sika_engine/SikaEngineApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SikaEngineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SikaEngineApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SikaEngineApplication.class, args);
+  }
 }
