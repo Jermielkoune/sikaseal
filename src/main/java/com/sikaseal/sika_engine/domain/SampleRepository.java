da72a8952e5b55fb91d3a2a77e73822247ba1193
KOUNOUHO Kpessou Jermiel <kpessou-jermiel.kounouho@epitech.eu>
2026-02-19 14:40:48 +0100

    add SampleRepository interface for data access layer

---
+++ src/main/java/com/sikaseal/sika_engine/domain/SampleRepository.java
@@ -0,0 +1,5 @@
+package com.sikaseal.sika_engine.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SampleRepository extends JpaRepository<SampleEntity, Long> {}
