d13325aa85145e62b0ae18f094be607c8a256478
KOUNOUHO Kpessou Jermiel <kpessou-jermiel.kounouho@epitech.eu>
2026-03-06 02:07:20 +0100

    add security configuration, custom properties, and DemoController for workout endpoint

--- src/main/java/com/sikaseal/sika_engine/domain/SampleRepository.java
+++
@@ -1,5 +0,0 @@
-package com.sikaseal.sika_engine.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SampleRepository extends JpaRepository<SampleEntity, Long> {}
