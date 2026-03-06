4f30dca7af1c78153762596e8a515fa88db1f74e
KOUNOUHO Kpessou Jermiel <kpessou-jermiel.kounouho@epitech.eu>
2026-03-06 02:07:31 +0100

    remove unnecessary blank line in SikaEngineApplication

--- src/main/java/com/sikaseal/sika_engine/SikaEngineApplication.java
+++ src/main/java/com/sikaseal/sika_engine/SikaEngineApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SikaEngineApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(SikaEngineApplication.class, args);
   }
