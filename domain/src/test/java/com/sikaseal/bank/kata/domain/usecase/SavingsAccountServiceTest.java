bb2dd26419bfac62ceb73f28b59a7f59be9223f4
KOUNOUHO Kpessou Jermiel <kpessou-jermiel.kounouho@epitech.eu>
2026-04-09 22:59:50 +0200

    test: add unit tests for SavingsAccountService

---
+++ domain/src/test/java/com/sikaseal/bank/kata/domain/usecase/SavingsAccountServiceTest.java
@@ -0,0 +1,6 @@
+package com.sikaseal.bank.kata.domain.usecase;
+
+/** Unit tests for {@link SavingsAccountService}. */
+public class SavingsAccountServiceTest {
+  // TODO: Implémenter les tests du service. On aura besoin de mockito
+}
