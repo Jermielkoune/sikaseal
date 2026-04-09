a8d93c0a5a17ba4a8e06d51a0f2f2a5c71fe205e
KOUNOUHO Kpessou Jermiel <kpessou-jermiel.kounouho@epitech.eu>
2026-04-09 22:54:39 +0200

    test: add unit tests for AccountService

---
+++ domain/src/test/java/com/sikaseal/bank/kata/domain/usecase/AccountServiceTest.java
@@ -0,0 +1,6 @@
+package com.sikaseal.bank.kata.domain.usecase;
+
+/** Unit tests for {@link AccountService}. */
+public class AccountServiceTest {
+  // TODO: Implémenter les tests du service. On aura besoin de mockito
+}
