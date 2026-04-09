b486c9cd8103700dd7daaece5cf3239a3d387a63
KOUNOUHO Kpessou Jermiel <kpessou-jermiel.kounouho@epitech.eu>
2026-04-09 22:58:26 +0200

    feat: add unit tests for GetMonthlyStatementService

---
+++ domain/src/test/java/com/sikaseal/bank/kata/domain/usecase/GetMonthlyStatementServiceTest.java
@@ -0,0 +1,6 @@
+package com.sikaseal.bank.kata.domain.usecase;
+
+/** Unit tests for {@link GetMonthlyStatementService}. */
+public class GetMonthlyStatementServiceTest {
+  // TODO: Implémenter les tests du service. On aura besoin de mockito
+}
