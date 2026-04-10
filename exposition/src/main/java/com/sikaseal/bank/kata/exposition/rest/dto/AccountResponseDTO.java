8932c80279bc454de7350003e61aef201647895b
KOUNOUHO Kpessou Jermiel <kpessou-jermiel.kounouho@epitech.eu>
2026-04-10 15:43:31 +0200

    refactor: simplify AccountResponseDTO record definition

--- exposition/src/main/java/com/sikaseal/bank/kata/exposition/rest/dto/AccountResponseDTO.java
+++ exposition/src/main/java/com/sikaseal/bank/kata/exposition/rest/dto/AccountResponseDTO.java
@@ -3,8 +3,4 @@ package com.sikaseal.bank.kata.exposition.rest.dto;
 import java.math.BigDecimal;
 
 public record AccountResponseDTO(
-        String accountId,
-        BigDecimal balance,
-        String accountType,
-        BigDecimal authorizedOverdraft
-) {}
+    String accountId, BigDecimal balance, String accountType, BigDecimal authorizedOverdraft) {}
